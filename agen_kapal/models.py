59ffda2b809abadc8df4dd9540adeb815e88160f
wellyfishy <wellyfishy@gmail.com>
2025-11-25 21:22:00 +0800

    database bit

--- agen_kapal/models.py
+++ agen_kapal/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+
+# Untuk Web-profil
+class Tentang(models.Model):
+    tentang = models.TextField()
+    last_edited = models.DateTimeField(null=True, blank=True)
+
+class Karyawan(models.Model):
+    nama_karyawan = models.CharField(max_length=100, null=True, blank=True)
+
+# Untuk panel admin
+class Kapal(models.Model):
+    imo = models.CharField(max_length=50, null=True, blank=True)
+
+class Client(models.Model):
+    nama_client = models.CharField(max_length=100, null=True, blank=True)
+    kapal = models.ForeignKey(Kapal, on_delete=models.SET_NULL, null=True, blank=True)
+    no_hp = models.CharField(max_length=20)
+    file = models.FileField(upload_to='berkas_client/', null=True, blank=True)
+
+class Request(models.Model):
+    STATUS = [
+        ('0', 'Menunggu'),
+        ('1', 'Diproses'),
+        ('2', 'Selesai'),
+        ('3', 'Dibatalkan')
+    ]
+
+    detail = models.TextField()
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(choices=STATUS, default='0')
+
+class FileKategori(models.Model):
+    nama_kategori = models.CharField(max_length=50, null=True, blank=True)
+
+class File(models.Model):
+    kapal = models.ForeignKey(Kapal, on_delete=models.CASCADE, null=True, blank=True)
+    request = models.ForeignKey(Request, on_delete=models.SET_NULL, null=True, blank=True)
+    file = models.FileField(upload_to='berkas_kapal/', null=True, blank=True)
+    kategori = models.ForeignKey(FileKategori, on_delete=models.SET_NULL, null=True, blank=True)
